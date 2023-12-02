8cca0571c834273259af89584c0772577f64e1bc
Hristina <hristinastankova89@gmail.com>
2023-12-02 12:04:09 +0100

    Creating twig for categories

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/categories', name: 'list_categories')]
+    public function list(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+    #[Route('/categories/{id<\d+>}', name: 'category_item')]
+    public function item(CategoryRepository $categoryRepository, int $id): Response
+    {
+        $category = $categoryRepository ->find($id);
+
+        if ($category === null) {
+
+            throw new FileNotFoundException('Not found');
+        }
+
+        return $this->render('category/item.html.twig', [
+            'category' => $category,
+            
+        ]);
+
+    }}
