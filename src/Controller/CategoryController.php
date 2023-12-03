9e33f3eaa67df9daef5f47cd3dbfe273dc475e32
Hristina <hristinastankova89@gmail.com>
2023-12-03 11:54:52 +0100

    Creating the  form and verifying if it's submitted and valide

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,26 +24,29 @@ class CategoryController extends AbstractController
     #[Route('/categories/{id<\d+>}', name: 'category_item')]
     public function item(CategoryRepository $categoryRepository, int $id): Response
     {
-        $category = $categoryRepository ->find($id);
-
+        $category = $categoryRepository->find($id);
         if ($category === null) {
-
             throw new FileNotFoundException('Not found');
         }
-
         return $this->render('category/item.html.twig', [
             'category' => $category,
-            
         ]);
 
     }
     #[Route('/new', name: 'new_category')]
-    public function new(): Response
-
-    {$category = new Category();
+    public function new(Request $request, EntityManagerInterface $entity): Response
+    {
+        $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
-        return $this->render('category/new.html.twig',
-    ['form_cat' => $form]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entity->persist($category);
+            $entity->flush();
+        }
+        return $this->renderForm(
+            'category/new.html.twig',
+            ['form_cat' => $form]
+        );
     }
 
 }
